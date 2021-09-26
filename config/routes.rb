a533749e15e42f75eb31f94ccc43ca31038990e6
yuki takagi <yuki99takagi@gmail.com>
2021-09-26 15:27:11 +0900

    add: users

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'profiles/show'
+  get 'profiles/edit'
+  get 'profiles/update'
   get "/books/:id" => "books#show"
   delete "/books/:id" => "books#destroy"
+  resources :publishers # resources 複数存在するリソースに使用する
+  resource :profile, only: %i{show edit update}
 end
