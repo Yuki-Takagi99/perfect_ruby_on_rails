ef3857bda3bd6a471c5681a5b6d94527ba2c5579
yuki takagi <yuki99takagi@gmail.com>
2021-09-26 12:34:12 +0900

    add: callback sample

--- app/models/book.rb
+++ app/models/book.rb
@@ -10,9 +10,25 @@ class Book < ApplicationRecord
   validates :name, presence: true
   validates :name, length: { maximum: 25 }
   validates :price, numericality: { greater_than_or_equal_to: 0 }
-  validate do |book|
-    if book.name.include?("exercise")
-      book.errors[:name] << "I don't like exercise."
+
+  before_validation :add_lovely_to_cat
+
+  after_destroy do
+    Rails.logger.info "Book is deleted: #{self.attributes}"
+  end
+
+  def add_lovely_to_cat
+    self.name = self.name.gsub(/Cat/) do |matched|
+      "lovely #{matched}"
     end
+  end
+
+  after_destroy :if => :high_price? do
+    Rails.logger.warn "Book with high price is deleted: #{self.attributes}"
+    Rails.logger.warn "Please check!!"
+  end
+
+  def high_price?
+    price >= 5000
   end
 end
